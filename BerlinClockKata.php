59fe5bc34fba5b6fbce4e51e5233b72f5db9d007
oussama.elbouenani <oussama.elbouenani@student.vinci.be>
2020-11-02 19:13:41 +0100

    add - time convertor simples minutes line including tests & methods refactor - simplifying code adding transform method

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -4,9 +4,25 @@
 class BerlinClockKata
 {
 
-    public function testMethod($int):int
+    public function convertToBerlinTime($time):String
     {
-        return 1;
+
+        preg_match("/([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2})/", $time, $match);
+        $hour = $match[1];
+        $min = $match[2];
+        $sec = $match[3];
+
+        var_dump("Recu -> ".$min);
+
+        return $this->countMinutes($min);
+    }
+
+    public function countMinutes(int $int):String{
+        if ($int % 5 == 0) return "[x][x][x][x]";
+        if ($int % 5 == 1) return "[Y][x][x][x]";
+        if ($int % 5 == 2) return "[Y][Y][x][x]";
+        if ($int % 5 == 3) return "[Y][Y][Y][x]";
+        if ($int % 5 == 4) return "[Y][Y][Y][Y]";
     }
 
 }
--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -7,13 +7,45 @@ use PHPUnit\Framework\TestCase;
 
 class BerlinClockKataTest extends TestCase
 {
-    public function test (){
 
-        $myclass = new BerlinClockKata();
+    private $berlinClock;
 
-        $actual = $myclass->testMethod(1);
-
-        assertEquals(1, $actual);
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->berlinClock = new BerlinClockKata();
     }
+
+    public function transform(String $str):String{
+        return $this->berlinClock->convertToBerlinTime($str);
+    }
+
+    /**SimpleMinutes**/
+
+    public function test_0minute_shouldReturn_Empty(){
+        $actual = $this->transform("00:00:00");
+
+        $this->assertEquals("[x][x][x][x]", $actual);
+    }
+
+    public function test_1minute_shouldReturn_Yxxx(){
+        $actual = $this->transform("00:01:00");
+
+        $this->assertEquals("[Y][x][x][x]", $actual);
+    }
+
+    public function test_4minutes_shouldReturn_YYYY(){
+        $actual = $this->transform("00:04:00");
+
+        $this->assertEquals("[Y][Y][Y][Y]", $actual);
+    }
+
+    public function test_5minutes_shouldReturn_xxxx(){
+        $actual = $this->transform("00:05:00");
+
+        $this->assertEquals("[x][x][x][x]", $actual);
+    }
+
+    /**5minutesBlocs**/
 
 }
