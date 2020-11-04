ed384a3bafb2742fed7d69d145ad161f010d126d
Loubnae0 <loubna.eljattari@student.vinci.be>
2020-11-04 13:15:45 +0100

    add function count for second minutes line and tests

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -18,11 +18,33 @@ class BerlinClockKata
     }
 
     public function countMinutes(int $int):String{
-        if ($int % 5 == 0) return "[x][x][x][x]";
-        if ($int % 5 == 1) return "[Y][x][x][x]";
-        if ($int % 5 == 2) return "[Y][Y][x][x]";
-        if ($int % 5 == 3) return "[Y][Y][Y][x]";
-        if ($int % 5 == 4) return "[Y][Y][Y][Y]";
+
+        $string = $this->countFiveMinutes($int/5);
+        $int=$int%5;
+
+        var_dump($string);
+        var_dump($int);
+
+        if ($int % 5 == 0) return $string."\n"."[x][x][x][x]";
+        if ($int % 5 == 1) return $string."\n"."[Y][x][x][x]";
+        if ($int % 5 == 2) return $string."\n"."[Y][Y][x][x]";
+        if ($int % 5 == 3) return $string."\n"."[Y][Y][Y][x]";
+        if ($int % 5 == 4) return $string."\n"."[Y][Y][Y][Y]";
+    }
+
+    public function countFiveMinutes(int $int):String{
+        if ($int == 0) return "[x][x][x][x][x][x][x][x][x][x][x]";
+        if ($int == 1) return "[Y][x][x][x][x][x][x][x][x][x][x]";
+        if ($int == 2) return "[Y][Y][x][x][x][x][x][x][x][x][x]";
+        if ($int == 3) return "[Y][Y][R][x][x][x][x][x][x][x][x]";
+        if ($int == 4) return "[Y][Y][R][Y][x][x][x][x][x][x][x]";
+        if ($int == 5) return "[Y][Y][R][Y][Y][x][x][x][x][x][x]";
+        if ($int == 6) return "[Y][Y][R][Y][Y][R][x][x][x][x][x]";
+        if ($int == 7) return "[Y][Y][R][Y][Y][R][Y][x][x][x][x]";
+        if ($int == 8) return "[Y][Y][R][Y][Y][R][Y][Y][x][x][x]";
+        if ($int == 9) return "[Y][Y][R][Y][Y][R][Y][Y][R][x][x]";
+        if ($int == 10) return"[Y][Y][R][Y][Y][R][Y][Y][R][Y][x]";
+        if ($int == 11) return"[Y][Y][R][Y][Y][R][Y][Y][R][Y][Y]";
     }
 
 }
--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -25,39 +25,92 @@ class BerlinClockKataTest extends TestCase
     public function test_0minute_shouldReturn_Empty(){
         $actual = $this->transform("00:00:00");
 
-        $this->assertEquals("[x][x][x][x]", $actual);
+        $this->assertEquals(
+            "[x][x][x][x][x][x][x][x][x][x][x]\n[x][x][x][x]",
+            $actual);
     }
 
     public function test_1minute_shouldReturn_Yxxx(){
         $actual = $this->transform("00:01:00");
 
-        $this->assertEquals("[Y][x][x][x]", $actual);
+        $this->assertEquals(
+            "[x][x][x][x][x][x][x][x][x][x][x]\n[Y][x][x][x]",
+            $actual);
     }
 
     public function test_2minute_shouldReturn_Yxxx(){
         $actual = $this->transform("00:02:00");
 
-        $this->assertEquals("[Y][Y][x][x]", $actual);
+        $this->assertEquals(
+            "[x][x][x][x][x][x][x][x][x][x][x]\n[Y][Y][x][x]",
+            $actual);
     }
 
     public function test_3minute_shouldReturn_Yxxx(){
         $actual = $this->transform("00:03:00");
 
-        $this->assertEquals("[Y][Y][Y][x]", $actual);
+        $this->assertEquals("[x][x][x][x][x][x][x][x][x][x][x]\n[Y][Y][Y][x]",
+            $actual);
     }
 
     public function test_4minutes_shouldReturn_YYYY(){
         $actual = $this->transform("00:04:00");
 
-        $this->assertEquals("[Y][Y][Y][Y]", $actual);
+        $this->assertEquals("[x][x][x][x][x][x][x][x][x][x][x]\n[Y][Y][Y][Y]", $actual);
     }
 
     public function test_5minutes_shouldReturn_xxxx(){
         $actual = $this->transform("00:05:00");
 
-        $this->assertEquals("[x][x][x][x]", $actual);
+        $this->assertEquals("[Y][x][x][x][x][x][x][x][x][x][x]\n[x][x][x][x]", $actual);
     }
 
     /**5minutesBlocs**/
+
+    /**FiveminutesBlocs**/
+    public function testFiveMinutesShouldReturnOneLightYellowAndTenLightOff() {
+        $actual = $this->transform("00:05:00");
+        $this->assertEquals("[Y][x][x][x][x][x][x][x][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testTenMinutesShouldReturnTwoLightYellowAndNineLightOff() {
+             $actual = $this->transform("00:10:00");
+             $this->assertEquals("[Y][Y][x][x][x][x][x][x][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testFifteenMinutesShouldReturnTwoLightYellowAndOneLightRedAndEightLightOff() {
+        $actual = $this->transform("00:15:00");
+        $this->assertEquals("[Y][Y][R][x][x][x][x][x][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testTwentyMinutesShouldReturnThreeLightYellowAndOneLightRedAndSeventLightOff() {
+         $actual = $this->transform("00:20:00");
+         $this->assertEquals("[Y][Y][R][Y][x][x][x][x][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testTwentyFiveMinutesShouldReturnFourLightYellowAndOneLightRedAndSixLightOff() {
+        $actual = $this->transform("00:25:00");
+        $this->assertEquals("[Y][Y][R][Y][Y][x][x][x][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testThirtyMinutesShouldReturnFourLightYellowAndTwoLightRedAndFiveLightOff() {
+         $actual = $this->transform("00:30:00");
+         $this->assertEquals("[Y][Y][R][Y][Y][R][x][x][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testThirtyFiveMinutesShouldReturnFiveLightYellowAndTwoLightRedAndFourLightOff() {
+        $actual = $this->transform("00:35:00");
+        $this->assertEquals("[Y][Y][R][Y][Y][R][Y][x][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testFourtyMinutesShouldReturnSixLightYellowAndTwoLightRedAndThreeLightOff() {
+         $actual = $this->transform("00:40:00");
+         $this->assertEquals("[Y][Y][R][Y][Y][R][Y][Y][x][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testFourtyFiveMinutesShouldReturnSixLightYellowAndThreeLightRedAndTwoLightOff() {
+        $actual = $this->transform("00:45:00");
+        $this->assertEquals("[Y][Y][R][Y][Y][R][Y][Y][R][x][x]\n[x][x][x][x]",$actual);
+    }
+    public function testFiftyMinutesShouldReturnSevenLightYellowAndThreeLightRedAndOneLightOff() {
+         $actual = $this->transform("00:50:00");
+         $this->assertEquals("[Y][Y][R][Y][Y][R][Y][Y][R][Y][x]\n[x][x][x][x]",$actual);
+    }
+    public function testFiftyFiveMinutesShouldReturnEightLightYellowAndThreeLightRed() {
+        $actual = $this->transform("00:55:00");
+        $this->assertEquals("[Y][Y][R][Y][Y][R][Y][Y][R][Y][Y]\n[x][x][x][x]",$actual);
+    }
 
 }
